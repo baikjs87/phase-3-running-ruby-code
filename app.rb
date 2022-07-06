dcff529f918f62a6d240fd1aef958fa6310b1aa5
Jason Baik <baikjs87@gmail.com>
2022-07-05 23:11:07 -0400

    Passed Test

---
+++ app.rb
@@ -0,0 +1,15 @@
+# To pass the rspec test
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
+
+# Extra stuffs from the module
+puts [1, 2, 3]
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }].pretty_inspect
+
+# Ruby comes with a tool called IRB (Interactive Ruby) for running a Ruby REPL (read-evaluate-print-loop) in the terminal, which provides similar functionality to the browser console that you're familiar with from JavaScript.
+# Using IRB is a great way to quickly test out some code, or check your syntax, without needing to run an entire application.
+
+
+
